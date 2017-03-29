4eed377da592f261653d74eaee72be5af634407a
Jean-David Flament <flamentjeandavid@yahoo.fr>
2017-03-29 10:17:32 +0200

    user profil update

--- pandiz/app/Views/Welcome/utilisateur.php
+++ pandiz/app/Views/Welcome/utilisateur.php
@@ -1,13 +1,13 @@
 <div id="banner-user" style="background: url(img/rome3.jpg); background-size:cover; background-position: center; border:1px solid black;">
-    <input type="file" class="hidden"/>
-    <label for="files" id="btn-add-banner" >Ajouter une bannière</label>
+    <input type="file" id="files-banner" name="upload-banner" class="hidden"/>
+    <label for="files-banner" id="btn-add-banner" >Ajouter une bannière</label>
 </div>
 
 <div id="profil-photo-name">
     <div id="profil-user" style="background: url(img/rome3.jpg); background-size:cover; background-position: center;">
         <div id="hover-profil-user">
-            <input type="file" class="hidden"/>
-            <label for="files" id="btn-add-photo-profil" >+</label>
+            <input type="file" id="files-photo" name="upload-photo-profil" class="hidden"/>
+            <label for="files-photo" id="btn-add-photo-profil" >+</label>
         </div>
     </div>
     
@@ -15,3 +15,20 @@
         <span id="profil-name"><?= $user->realname ?></span><span> - </span><span id="profil-username"><?= $user->username ?></span>
     </div>
 </div>
+
+<div style="clear:both;"></div>
+
+<div id="onglets-utilisateur">
+        <div id="informations-utilisateur">
+            <p>Informations</p>
+        </div>
+        <div id="playlists-utilisateur">
+            <p>Playlists</p>
+        </div>
+        <div id="titres-utilisateur">
+            <p>Titres</p>
+        </div>
+        <div id="albums-utilisateur">
+            <p>Albums</p>
+        </div>
+    </div>
