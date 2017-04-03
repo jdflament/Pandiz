e3c05009775c5a6a5a14790f78eb91bbe9d5e71c
clementdelannoy1c1 <delannoy.clement08@gmail.com>
2017-04-03 13:01:34 +0200

    restriction ok

--- pandiz/app/Views/Welcome/utilisateur.php
+++ pandiz/app/Views/Welcome/utilisateur.php
@@ -1,18 +1,25 @@
 <div id="banner-user" style="background: url('<?= $user->banner ?>'); background-size:cover; background-position: center;">
-    <form id='formbanner'  method="post" enctype="multipart/form-data" action="/banner/cree">
-        <input type="file" id="files-banner" name="upload-banner" class="hidden"/>
-        <label for="files-banner" id="btn-add-banner" >Ajouter une bannière</label>
-    </form>
+    <?php
+        if($user->id === Auth::id()){
+            echo "<form id='formbanner'  method='post' enctype='multipart/form-data' action='/banner/cree'>
+        <input type='file' id='files-banner' name='upload-banner' class='hidden'/>
+        <label for='files-banner' id='btn-add-banner' >Ajouter une bannière</label>
+    </form>";
+        } ?>
 </div>
 
 <div id="profil-photo-name">
     <div id="profil-user" style="background-image: url('<?= $user->avatar ?>'); background-size:cover; background-position: center;">
-        <div id="hover-profil-user">
-            <form id='formavatar'  method="post" enctype="multipart/form-data" action="/avatar/cree">
-            <input type="file" id="files-photo" name="upload-photo-profil" class="hidden" />
-            <label for="files-photo" id="btn-add-photo-profil" >+</label>
-            </form>
-            </div>
+        <?php
+            if($user->id === Auth::id()){
+                echo "
+                    <div id='hover-profil-user'>
+                    <form id='formavatar'  method='post' enctype='multipart/form-data' action='/avatar/cree'>
+                    <input type='file' id='files-photo' name='upload-photo-profil' class='hidden' />
+                    <label for='files-photo' id='btn-add-photo-profil' >+</label>
+                    </form>
+                    </div>";
+            } ?>
     </div>
     
     <div id="profil-name-username">
